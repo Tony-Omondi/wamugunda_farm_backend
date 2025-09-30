a35c6043191e7a310a83b1ced8d0befc8d9a2576
Tony-Omondi <otienotony598@gmail.com>
2025-09-30 20:43:24 +0300

    commited

--- farm_backend/urls.py
+++ farm_backend/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('api/', include('core.urls')),  # All core APIs under /api/
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
